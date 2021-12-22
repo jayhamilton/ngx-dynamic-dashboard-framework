e1deb7e2f6026ebf26e3ab3aa9e7ecfd02bf4865
Jay Hamilton <jaystevenhamilton@gmail.com>
2021-12-21 22:20:31 -0500

    update to drag and drop to include persisting changes to local storage

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {
   CdkDragDrop,
+  CdkDropList,
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+
+import { BoardService } from './board.service';
 
 @Component({
   selector: 'app-board',
@@ -11,48 +14,13 @@ import {
   styleUrls: ['./board.component.css'],
 })
 export class BoardComponent {
-
-  columns = [
-    {
-      width: 30,
-      gadgetNames:['image1', 'image2'],
-      gadgets: [
-        {
-          name: 'image1',
-        },
-        {
-          name: 'image2',
-        },
-      ],
-    },
-    {
-      width: 30,
-      gadgetNames:['image3', 'image4'],
-      gadgets: [
-        {
-          name: 'image3',
-        },
-        {
-          name: 'image4',
-        },
-      ],
-    },
-    {
-      width: 30,
-      gadgetNames:['image5', 'image6'],
-      gadgets: [
-        {
-          name: 'image5',
-        },
-        {
-          name: 'image6',
-        },
-      ],
-    }
-  ];
+  boardData: any;
+  constructor(private boardService: BoardService) {
+    this.boardData = this.boardService.getDefaultData();
+  }
 
   drop(event: CdkDragDrop<string[]>) {
-    console.log(event);
+    //console.log(event);
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
@@ -67,9 +35,29 @@ export class BoardComponent {
         event.currentIndex
       );
     }
+    this.updateDataModel(event.container, event.previousContainer);
   }
 
-  getImage(colIdx: number, gadgetIdx: number){
-    return "/assets/images/img" + gadgetIdx;
+  getImage(colIdx: number, gadgetIdx: number) {
+    return '/assets/images/img' + gadgetIdx;
+  }
+
+  getColumnIndexAsString(idx: number) {
+    return '' + idx;
+  }
+
+  updateDataModel(container: CdkDropList, previousContainer: CdkDropList) {
+    let cIdx = parseInt(container.id);
+    let pIdx = parseInt(previousContainer.id);
+
+    //this means a component was moved from one column to another
+    if (cIdx != pIdx) {
+      this.boardData[pIdx].gadgetNames = previousContainer.data;
+    }
+
+    this.boardData[cIdx].gadgetNames = container.data;
+
+    //persist the change
+    this.boardService.write(this.boardData);
   }
 }
--- src/app/board/board.service.ts
+++ src/app/board/board.service.ts
@@ -1,21 +1,66 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class BoardService {
-
-  apiUrl : string = 'board';
-  headers = new HttpHeaders().set('Content-Type', 'application/json');
-
-  constructor( private http: HttpClient) { }
-
-  getByName(name:string){
-    return this.http.get(`${this.apiUrl}`);
+  public read() {
+    return localStorage.getItem('plmBoard');
+  }
+  public write(boardData: any) {
+    localStorage.removeItem('plmBoard');
+    localStorage.setItem('plmBoard', JSON.stringify(boardData));
   }
 
+  public getDefaultData() {
+    let data = [
+      {
+        width: 30,
+        gadgetNames: ['img1', 'img2'],
+        gadgets: [
+          {
+            name: 'img1',
+          },
+          {
+            name: 'img2',
+          },
+        ],
+      },
+      {
+        width: 30,
+        gadgetNames: ['img3', 'img4'],
+        gadgets: [
+          {
+            name: 'img3',
+          },
+          {
+            name: 'img4',
+          },
+        ],
+      },
+      {
+        width: 30,
+        gadgetNames: ['img5', 'img6'],
+        gadgets: [
+          {
+            name: 'img5',
+          },
+          {
+            name: 'img6',
+          },
+        ],
+      },
+    ];
 
+    if (this.read() == null) {
+      this.write(data);
+    }
+
+    let _data = this.read();
+    if (_data == null) {
+      _data = '';
+    }
+
+    return JSON.parse(_data);
+  }
 }
