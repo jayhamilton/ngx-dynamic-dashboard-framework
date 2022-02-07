f741abe904f16533478b383247316827b4ebeb11
Jay Hamilton <jaystevenhamilton@gmail.com>
2022-02-06 22:54:24 -0500

    Close the libary gadget when an item is added.

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { EventService } from '../eventservice/event.service';
 import { IGadget } from '../gadgets/common/gadget-common/gadget-base/gadget.model';
 import { LibraryService } from './library.service';
@@ -6,30 +12,33 @@ import { LibraryService } from './library.service';
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
-  styleUrls: ['./library.component.css']
+  styleUrls: ['./library.component.css'],
 })
-export class LibraryComponent implements OnInit {
+export class LibraryComponent implements OnInit, AfterViewInit {
+  @ViewChild('dialog', { read: ElementRef })
+  libraryDialogCloseButton?: ElementRef;
 
-  constructor(private libraryService: LibraryService, private eventService:EventService) { }
+  constructor(
+    private libraryService: LibraryService,
+    private eventService: EventService
+  ) {}
+  ngAfterViewInit(): void {
+    console.log(this.libraryDialogCloseButton);
+  }
 
   library!: IGadget[];
   ngOnInit(): void {
-
     this.getLibrary();
   }
 
-  getLibrary(){
-
-    this.libraryService.getLibrary().subscribe((libraryData)=>{
+  getLibrary() {
+    this.libraryService.getLibrary().subscribe((libraryData) => {
       this.library = libraryData;
     });
   }
 
-  addGadget(gadgetData: IGadget){
-
-    this.eventService.emitLibraryAddGadgetEvent({data:gadgetData})
-
-
+  addGadget(gadgetData: IGadget) {
+    this.eventService.emitLibraryAddGadgetEvent({ data: gadgetData });
+    this.libraryDialogCloseButton?.nativeElement.click();
   }
-
 }
