75d44f62cf96355ed1689d5071bc782c825c6541
Jay Hamilton <jaystevenhamilton@gmail.com>
2022-02-13 13:01:32 -0500

    #12 Logic added to select the layout icon upon board selection.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../board/board.service';
 import { EventService } from '../eventservice/event.service';
 import { layouts, LayoutType } from './layout.model';
 
@@ -10,13 +11,19 @@ import { layouts, LayoutType } from './layout.model';
 export class SidelayoutComponent implements OnInit {
   _layouts = layouts;
   selectedLayoutId = -1;
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, boardService: BoardService) {
+    eventService.listenForBoardSelectedEvent().subscribe((event) => {
+      //get the board and layout then get the id and set it.
+      boardService.getBoardById(event.data).subscribe((board) => {
+        //console.log('Board structure of the selected board!');
+        //console.log(board.structure);
 
-    eventService.listenForBoardSelectedEvent().subscribe((board)=>{
-
-      console.log("Board selected so select layout");
-      console.log(board);
-
+        layouts.forEach((layout) => {
+          if (layout.structure.localeCompare(board.structure) == 0) {
+            this.selectedLayoutId = layout.id;
+          }
+        });
+      });
     });
   }
 
