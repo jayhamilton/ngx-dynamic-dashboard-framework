e069732b5fb214abfc01eb9d985faad61ef15363
Jay Hamilton <jaystevenhamilton@gmail.com>
2022-10-10 00:19:23 -0400

    Update to line gadget properties

--- src/app/gadgets/packaging-line/pck-line.component.ts
+++ src/app/gadgets/packaging-line/pck-line.component.ts
@@ -1,65 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { Color, ScaleType } from '@swimlane/ngx-charts';
 import { BoardService } from 'src/app/board/board.service';
 import { EventService } from 'src/app/eventservice/event.service';
 import { GadgetBase } from '../common/gadget-common/gadget-base/gadget.base';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { UserDataService } from 'src/app/dataservice/user.data.service';
 import { UntypedFormControl } from '@angular/forms';
-import { IUser } from 'src/app/configuration/tab-rbac/rbac.service';
-interface Food {
-  value: string;
-  viewValue: string;
-}
+
 @Component({
   selector: 'app-pck-line',
   templateUrl: './pck-line.component.html',
   styleUrls: ['./pck-line.component.scss']
 })
 export class PckLineComponent extends GadgetBase implements OnInit {
-  leads = new UntypedFormControl();
-  leadList: Array<string> = [];
 
-  constructor(private userDataService: UserDataService, private eventService: EventService, private boardService: BoardService) {
+  driver?: string;
+
+  constructor(private eventService: EventService, private boardService: BoardService) {
     super();
 
-    this.setupEventHandlers();
 
   }
-
 
   ngOnInit(): void {
-    this.getLeadsList();
-  }
 
-  setupEventHandlers(){
+    this.getDriver();
 
-    this.eventService.listenForUserDataChangedEvent().subscribe(() => {
-      this.getLeadsList();
-    });
   }
 
   remove() {
     this.eventService.emitGadgetDeleteEvent({ data: this.instanceId });
   }
 
-  getLeadsList() {
-    this.leadList.length = 0;
+  getLineLeads() {
+    let lineLeads: string[] = [];
 
-    this.userDataService.getUsers().forEach(record => {
+    this.propertyPages[0].properties.forEach((property) => {
 
-        if (record.roles.toLocaleLowerCase().localeCompare('lead') === 0) {
+      if (property.key == "lead") {
+        lineLeads = property.value;
+      }
 
-          this.leadList.push(record.username);
+    })
+    return lineLeads;
 
-        }
-
-      });
   }
+
+  getDriver() {
+
+    this.propertyPages[0].properties.forEach((property) => {
+
+      if (property.key == "driver") {
+        this.driver = property.value;
+      }
+
+    })
+  }
+
 
   propertyChangeEvent(propertiesJSON: string) {
     //update internal props
     const updatedPropsObject = JSON.parse(propertiesJSON);
+    console.log(updatedPropsObject);
 
     if (updatedPropsObject.title != undefined) {
       this.title = updatedPropsObject.title;
@@ -67,6 +67,12 @@ export class PckLineComponent extends GadgetBase implements OnInit {
     if (updatedPropsObject.subtitle != undefined) {
       this.subtitle = updatedPropsObject.subtitle;
       console.log.apply(this.subtitle);
+    }
+
+    if (updatedPropsObject.driver != undefined) {
+
+      this.getDriver();
+
     }
 
     //persist changes
