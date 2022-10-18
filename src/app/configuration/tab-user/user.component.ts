6f32883164e675c634ca654676c3a2e86485fe67
Jay Hamilton <jaystevenhamilton@gmail.com>
2022-10-17 21:55:11 -0400

    Component label and title changes

--- src/app/configuration/tab-user/user.component.ts
+++ src/app/configuration/tab-user/user.component.ts
@@ -27,7 +27,7 @@ export class TabUserComponent implements OnInit {
 
   roleList: string[] = ['Driver', 'Lead'];
 
-  displayedColumns: string[] = ['Id', 'User Name', 'Roles', 'Tools'];
+  displayedColumns: string[] = ['Id', 'Name', 'Role', 'Tools'];
   dataSource = new UserDataSource(ELEMENT_DATA);
   constructor(private userService: UserService, private userDataStoreService: UserDataStoreService, formBuilder: UntypedFormBuilder, private dialog: MatDialog) {
 
