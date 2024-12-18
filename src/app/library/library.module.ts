4e195d09178f0b474bff2001f5ccab6fcc0c36e4
Jay Hamilton <jaystevenhamilton@gmail.com>
2024-12-17 21:14:53 -0500

    angular 17 component updates

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibraryComponent } from './library.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { LibraryService } from './library.service';
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 
--- src/app/sidenav/sidenav.module.ts
+++ src/app/sidenav/sidenav.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BoardModule } from '../board/board.module';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { SidelayoutModule } from '../layout/layout.module';
 
 @NgModule({
