2ffd4bd0c1ded9fc5acf70e78b37a977e341c75b
Jay Hamilton <jaystevenhamilton@gmail.com>
2024-12-20 15:04:33 -0500

    Minor css tweaks

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -19,6 +19,14 @@ export class LibraryComponent implements OnInit, AfterViewInit {
   @ViewChild('dialog', { read: ElementRef })
   libraryDialogCloseButton?: ElementRef;
 
+  colors = [
+    '#FF5733', '#33FF57', '#3357FF', '#F1C40F', '#8E44AD', '#E74C3C',
+    '#3498DB', '#2ECC71', '#1ABC9C', '#9B59B6', '#34495E', '#16A085',
+    '#F39C12', '#D35400', '#C0392B', '#7F8C8D', '#BDC3C7', '#95A5A6',
+    '#2980B9', '#27AE60', '#8E44AD', '#2C3E50', '#F4D03F', '#E67E22',
+    '#D35400', '#1ABC9C', '#2ECC71', '#E74C3C', '#9B59B6', '#34495E'
+  ];
+
   constructor(
     private libraryService: LibraryService,
     private eventService: EventService
