4524baba7a7d332fcc30b1ce282a86d5d6e7a267
Jay Hamilton <jaystevenhamilton@gmail.com>
2026-07-30 21:15:58 -0400

    fix: replace radio-button nav list with styled nav list and persistent selection

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -16,7 +16,8 @@ export class SidenavComponent implements OnInit {
   @ViewChild('layout') public layout!: MatDrawer;
   boardData: IBoard[] = [];
 
-  showFiller = false;
+  selectedBoardId: number | null = null;
+
   constructor(
     private eventService: EventService,
     private boardService: BoardService
@@ -38,18 +39,23 @@ export class SidenavComponent implements OnInit {
   loadBoards() {
     this.boardService.getBoardCollection().subscribe((boardCollection: IBoardCollection) => {
       this.boardData = boardCollection.boardList.filter((obj) => {
-
-        return obj.relationship == Hiearchy.PARENT
+        return obj.relationship == Hiearchy.PARENT;
       });
+      if (!this.selectedBoardId) {
+        this.boardService.getLastSelectedBoard().subscribe((board: IBoard) => {
+          if (board && board.id != null) {
+            this.selectedBoardId = board.id;
+          } else if (this.boardData.length > 0) {
+            this.selectedBoardId = this.boardData[0].id;
+          }
+        });
+      }
     });
   }
 
-  showBoard(boardList: any) {
-    //raise event to show the selected board
-    let boardSelected = boardList.selectedOptions.selected[0]?.value;
-
-    this.eventService.emitBoardSelectedEvent({ data: boardSelected });
-
+  selectBoard(boardId: number) {
+    this.selectedBoardId = boardId;
+    this.eventService.emitBoardSelectedEvent({ data: boardId });
   }
 
   setupEventListeners() {
@@ -75,9 +81,11 @@ export class SidenavComponent implements OnInit {
         this.loadBoards();
       });
     this.eventService.listenForBoardUpdateNameDescriptionRequestEvent().subscribe((event) => {
+      this.loadBoards();
+    });
 
-     this.loadBoards();
-
+    this.eventService.listenForBoardSelectedEvent().subscribe((event) => {
+      this.selectedBoardId = event.data;
     });
   }
 }
