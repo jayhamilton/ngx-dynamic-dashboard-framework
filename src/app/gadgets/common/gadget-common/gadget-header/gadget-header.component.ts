a60d7162e21e046658aa788b0133f36e69b9ad5e
Jay Hamilton <jaystevenhamilton@gmail.com>
2026-08-08 10:42:16 -0400

    Replace gadget card overflow menu with standalone icons
    Configure and Remove are now their own icon buttons next to Help,
    matching that pattern, instead of living behind a vertical-ellipsis
    menu. Configure still hides when the board is locked.

--- src/app/gadgets/common/gadget-common/gadget-header/gadget-header.component.ts
+++ src/app/gadgets/common/gadget-common/gadget-header/gadget-header.component.ts
@@ -7,7 +7,6 @@ import { BoardService } from 'src/app/board/board.service';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 import { MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle } from '@angular/material/card';
 import { MatIconButton } from '@angular/material/button';
-import { MatMenuTrigger, MatMenu, MatMenuItem } from '@angular/material/menu';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
 import { IPropertyPage, ITag } from '../gadget-base/gadget.model';
@@ -18,7 +17,7 @@ import { AnimationService } from 'src/app/animation/animation.service';
     templateUrl: './gadget-header.component.html',
     styleUrls: ['./gadget-header.component.scss'],
     changeDetection: ChangeDetectionStrategy.Eager,
-    imports: [MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatIconButton, MatMenuTrigger, MatIcon, MatMenu, MatMenuItem, MatTooltip]
+    imports: [MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatIconButton, MatIcon, MatTooltip]
 })
 export class GadgetHeaderComponent implements OnInit, OnDestroy {
   @Output() removeEvent: EventEmitter<any> = new EventEmitter();
